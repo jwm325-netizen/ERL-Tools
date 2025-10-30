671796b964a07f346bc843cc029e5f79d1d0d3d6
jwm325-netizen <jwm325@lehigh.edu>
2025-10-30 16:49:41 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "My Site",
+        title: "Tools for Electronic Resources Librarians",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
