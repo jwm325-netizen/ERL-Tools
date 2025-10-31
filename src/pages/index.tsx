d22667e60dfe3dc56fa34b00ce5f31d2aaecd70b
jwm325-netizen <jwm325@lehigh.edu>
2025-10-31 12:20:33 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageMarkdown from 'markdown-page.md';
+import HomepageMarkdown from 'markdown-page.mdx';
 
 import styles from './index.module.css';
 
